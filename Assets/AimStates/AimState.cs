97c2f12f4a5afe9b05234400ea6cbdb6987f1398
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-24 16:15:55 +0000

    first commit

---
+++ Assets/AimStates/AimState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AimState : AimBaseState
+{
+    public override void EnterState(AimStateManager aim)
+    {
+        aim.anim.SetBool("Aiming", true);
+    }
+    public override void UpdateState(AimStateManager aim)
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1)) aim.SwitchState(aim.Hip);
+    }
+}
---
+++ Assets/AimStates/HipFireState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HipFireState : AimBaseState
+{
+    public override void EnterState(AimStateManager aim)
+    {
+        aim.anim.SetBool("Aiming", false);
+    }
+    public override void UpdateState(AimStateManager aim)
+    {
+        if (Input.GetKey(KeyCode.Mouse1)) aim.SwitchState(aim.Aim);
+    }
+}
